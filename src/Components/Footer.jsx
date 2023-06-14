d2549f6203f027ebee88c30d66d6c7385aa28e22
Arc <atandarokib@gmail.com>
2023-06-14 17:11:51 +0100

    up

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Icon } from "@iconify/react";
+
+const Footer = () => {
+  return (
+    <footer className="py-[20px] w-full bg-red-700">
+      <div className="container flex justify-center flex-col items-center">
+        <a href="" className="text-4xl md:text-5xl capitalize">
+          holy <span className="text-orange-400">raptor</span>
+        </a>
+        <div className="flex items-center gap-2">
+          <a href="" className="text-3xl  transition duration-1000">
+            <Icon icon="logos:ethereum-color" />
+          </a>
+          <a href="" className="text-5xl  transition duration-1000">
+            <Icon icon="openmoji:twitter" />{" "}
+          </a>
+          <a href="" className="text-2xl  transition duration-1000">
+            <Icon icon="logos:telegram" />{" "}
+          </a>
+        </div>{" "}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
